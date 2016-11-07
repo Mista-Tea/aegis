62d5413ef805906f6e5d6a2a588de1415a4f3af1
Mista-Tea <thatguythomas@hotmail.com>
2016-11-07 01:54:07 -0600

    Removing unused client directory includes

--- lua/autorun/aegis_init.lua
+++ lua/autorun/aegis_init.lua
@@ -32,7 +32,7 @@ local hook = hook
 
 --[[--------------------------------------------------------------------------
 --
--- 	aegis.Initialize( boolean )
+-- 	aegis.Initialize()
 --
 --]]--
 function aegis.Initialize()
@@ -41,14 +41,10 @@ function aegis.Initialize()
 		
 		utils.AddFiles( "aegis/server", "include" )
 		utils.AddFiles( "aegis/shared", "shared" )
-		utils.AddFiles( "aegis/client", "client" )
-
-
+		
 	elseif ( CLIENT ) then
 		
-		utils.AddFiles( "aegis/shared", "include" )
-		utils.AddFiles( "aegis/client", "include" )
-	
+		utils.AddFiles( "aegis/shared", "include" )	
 	end
 end
 hook.Add( "Initialize", "Aegis", aegis.Initialize )
